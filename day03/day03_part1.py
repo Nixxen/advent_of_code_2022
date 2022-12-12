9d03e2cb6cb755b3a8739b3a67cfdf210dc012e7
Odd Mathisen <oddoysteim@uia.no>
2022-12-12 19:23:11 +0100

    ✨ Day03 part 1 and part 2 complete

--- day03/day03_part1.py
+++ day03/day03_part1.py
@@ -1,5 +1,7 @@
-RUN_TEST = True
-TEST_SOLUTION = ...
+from collections import defaultdict
+
+RUN_TEST = False
+TEST_SOLUTION = 157
 TEST_INPUT_FILE = "test_input_day_03.txt"
 INPUT_FILE = "input_day_03.txt"
 
@@ -12,9 +14,32 @@ def main_part1(
     with open(input_file) as file:
         lines = list(map(lambda line: line.rstrip(), file.readlines()))
 
-    ...
+    # Each rucksack has two large compartments. All items of a given type are
+    # meant to go into exactly one of the two compartments.
 
-    solution = ...
+    # To help prioritize item rearrangement, every item type can be converted
+    # to a priority:
+
+    # Lowercase item types a through z have priorities 1 through 26.
+    # Uppercase item types A through Z have priorities 27 through 52.
+    def convert(char: str, count: int) -> int:
+        if char.islower():
+            return (ord(char) - ord("a") + 1) * count
+        return (ord(char) - ord("A") + 1 + 26) * count
+
+    # Find the item type that appears in both compartments of each rucksack.
+    # What is the sum of the priorities of those item types?
+
+    duplicates = defaultdict(int)
+    for line in lines:
+        left_compartment = line[: len(line) // 2]
+        right_compartment = line[len(line) // 2 :]
+        for char in left_compartment:
+            if char in right_compartment:
+                duplicates[char] += 1
+                break  # Do not count duplicates
+
+    solution = sum(map(convert, duplicates.keys(), duplicates.values()))
     return solution
 
 
--- day03/day03_part2.py
+++ day03/day03_part2.py
@@ -1,5 +1,7 @@
-RUN_TEST = True
-TEST_SOLUTION = ...
+from collections import defaultdict
+
+RUN_TEST = False
+TEST_SOLUTION = 70
 TEST_INPUT_FILE = "test_input_day_03.txt"
 INPUT_FILE = "input_day_03.txt"
 
@@ -12,9 +14,28 @@ def main_part2(
     with open(input_file) as file:
         lines = list(map(lambda line: line.rstrip(), file.readlines()))
 
-    ...
+    # The only way to tell which item type is the right one is by finding the
+    # one item type that is common between all three Elves in each group.
 
-    solution = ...
+    # One group of elves are represented by the three lines containing their
+    # backpacks.
+
+    # Find the item type that corresponds to the badges of each three-Elf
+    # group. What is the sum of the priorities of those item types?
+
+    def convert(char: str, count: int) -> int:
+        if char.islower():
+            return (ord(char) - ord("a") + 1) * count
+        return (ord(char) - ord("A") + 1 + 26) * count
+
+    badges = defaultdict(int)
+    for index in range(0, len(lines), 3):
+        for char in lines[index]:
+            if char in lines[index + 1] and char in lines[index + 2]:
+                badges[char] += 1
+                break
+
+    solution = sum(map(convert, badges.keys(), badges.values()))
     return solution
 
 
