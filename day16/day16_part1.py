ba6291d3cc682c155106561a1cac211c93060c4f
Odd Mathisen <oddoysteim@uia.no>
2022-12-22 12:17:50 +0100

    🚧 Day16 part 1 progress Solves example, but too slow for main puzzle

--- day16/day16_part1.py
+++ day16/day16_part1.py
@@ -1,7 +1,9 @@
 import re
 from collections import deque
+from dataclasses import dataclass
+from functools import total_ordering
 
-RUN_TEST = True
+RUN_TEST = False
 TEST_SOLUTION = 1651
 TEST_INPUT_FILE = "test_input_day_16.txt"
 INPUT_FILE = "input_day_16.txt"
@@ -9,29 +11,24 @@ INPUT_FILE = "input_day_16.txt"
 ARGS = []
 
 
-class Spelunker:
+@total_ordering
+class Journey:
+    """Container class for one individual path and time"""
+
     def __init__(
-        self, start_valve: "Valve", valves: dict[str, "Valve"], time_limit: int
+        self, path: list[tuple[str, str]], time: int, valves: dict[str, "Valve"]
     ) -> None:
-        self.start_valve = start_valve
+        self.path = path
+        self.time_left = time
+        self.visited, self.opened = self._get_state(path)
+        self.complete = False
+        self.pressure = 0
         self.valves = valves
-        self.time_limit = time_limit
-        self.start_valve = start_valve
-        self.start_time = time_limit
-        self.max_pressure = 0
-        # Valid actions: "enter" tunnel (cost 1 min), "open" valve (cost 1 min)
 
-    def is_feasible(self, valve: "Valve") -> bool:
-        return valve.flow_rate > 0
-
-    def find_pressure(self, valve: "Valve", time_left) -> int:
-        return valve.flow_rate * time_left
-
-    def calculate_pressure(self, path: list[tuple[str, str]], start_time: int) -> int:
+    def _calculate_pressure(self, start_time: int) -> int:
         """Calculate the pressure release of a path
 
         Args:
-            path (list[tuple[str, str]]): List of actions and valves
             start_time (int): Time to start calculating pressure
 
         Returns:
@@ -39,10 +36,10 @@ class Spelunker:
         """
         pressure = 0
         time_left = start_time
-        for action, valve in path[1:]:
+        for action, valve in self.path[1:]:
             if action == "open":
                 time_left -= 1
-                pressure += self.find_pressure(self.valves[valve], time_left)
+                pressure += self._find_pressure(self.valves[valve], time_left)
             elif action == "enter":
                 time_left -= 1
             else:
@@ -50,7 +47,16 @@ class Spelunker:
                 pass
         return pressure
 
-    def get_state(self, path: list[tuple[str, str]]) -> tuple[set[str], set[str]]:
+    def _find_pressure(self, valve: "Valve", time_left) -> int:
+        return valve.flow_rate * time_left
+
+    def complete_journey(self, start_time: int) -> None:
+        """Finalizes the journey, calculating the total pressure released
+        from the given start time"""
+        self.complete = True
+        self.pressure = self._calculate_pressure(start_time)
+
+    def _get_state(self, path: list[tuple[str, str]]) -> tuple[set[str], set[str]]:
         """Get the state of the valves based on the given path
 
         Args:
@@ -67,7 +73,38 @@ class Spelunker:
                 opened.add(valve)
         return visited, opened
 
-    def bfs(self, start_valve: str, target_valve: str) -> list[tuple[str, str]]:
+    def __str__(self) -> str:
+        return f"Journey(Path: {self.path}, Time left: {self.time_left}, Pressure: {self.pressure})"
+
+    def __eq__(self, __o: object) -> bool:
+        """Equality based on final pressure. Uncompleted journeys can not be
+        compared"""
+        if isinstance(__o, Journey):
+            return self.pressure == __o.pressure
+        return False
+
+    def __lt__(self, __o: object) -> bool:
+        """Less than based on final pressure. Uncompleted journeys can not be
+        compared"""
+        if isinstance(__o, Journey):
+            return self.pressure < __o.pressure
+        return False
+
+
+class Spelunker:
+    """Main explorer logic"""
+
+    def __init__(self, valves: dict[str, "Valve"], time_limit: int) -> None:
+        self.valves = valves
+        self.time_limit = time_limit
+        self.start_time = time_limit
+        self.max_pressure = 0
+        # Valid actions: "enter" tunnel (cost 1 min), "open" valve (cost 1 min)
+
+    def _is_feasible(self, valve: "Valve") -> bool:
+        return valve.flow_rate > 0
+
+    def _bfs(self, start_valve: str, target_valve: str) -> list[tuple[str, str]]:
         """BFS to find the shortest path from start_valve to target_valve
 
         Args:
@@ -92,9 +129,7 @@ class Spelunker:
                     queue.append(new_path)
         return []
 
-    def find_highest_pressure(
-        self, path: list[tuple[str, str]], time_left: int
-    ) -> list[tuple[str, str]]:
+    def find_highest_pressure(self, journey: "Journey") -> "Journey":
         """Find the actions to take that releases most pressure. DFS is used
         to find the highest possible pressure release in the shortest amount
         of time.
@@ -107,84 +142,99 @@ class Spelunker:
             list[str]: List of action (valve or tunnel) names in priority order
         """
         # Base case
-        if time_left == 0:
-            final_pressure = self.calculate_pressure(path, self.start_time)
-            path.append(("return", str(final_pressure)))
-            # print(f"Time limit reached for path {path}")
-            return path
+        if journey.time_left == 0:
+            journey.complete_journey(self.start_time)
+            # print(f"Time limit (pressure={journey.pressure}) reached for path {journey.path}")
+            return journey
 
-        # Get state of valves based on path, and find the valid next actions
-        visited, opened = self.get_state(path)
+        _, current_valve = journey.path[-1]
+        next_journeys: list["Journey"] = []
+        if not current_valve in journey.opened and self._is_feasible(
+            self.valves[current_valve]
+        ):
+            # Open current valve
+            new_path = list(journey.path)
+            new_path.append(("open", current_valve))
+            new_journey = Journey(new_path, journey.time_left - 1, self.valves)
+            next_journeys.append(new_journey)
+        else:
+            for valve in self.valves.values():
+                if (
+                    valve.valve != current_valve
+                    and self._is_feasible(valve)
+                    and valve.valve not in journey.opened
+                ):
+                    path_to_valve = self._bfs(current_valve, valve.valve)
+                    # print(f"Path to {valve.valve}: {path_to_valve}")
+                    if path_to_valve:
+                        # Extend path with path to valve
+                        path_to_valve.pop(0)
+                        new_path = list(journey.path)
+                        new_path.extend(path_to_valve)
+                        new_journey = Journey(
+                            new_path,
+                            journey.time_left - len(path_to_valve),
+                            self.valves,
+                        )
+                        next_journeys.append(new_journey)
 
-        _, current_valve = path[-1]
-        next_actions: list[tuple[str, str]] = []
-        if not current_valve in opened and self.is_feasible(self.valves[current_valve]):
-            next_actions.append(("open", current_valve))
-        for valve in self.valves.values():
-            if (
-                valve.valve != current_valve
-                and self.is_feasible(valve)
-                and valve.valve not in opened
-            ):
-                path_to_valve = self.bfs(current_valve, valve.valve)
-                print(f"Path to {valve.valve}: {path_to_valve}")
-                if path_to_valve:
-                    # Find first tuple with an enter action
-                    first_enter = next(
-                        i for i, x in enumerate(path_to_valve) if x[0] == "enter"
-                    )
-                    next_actions.append(path_to_valve[first_enter])
-                    # break
-                    # TODO: Make shortcut, instead of re-calculating
-
-        # If there are no valid actions in current valve room
-        if not next_actions:
+        if not next_journeys:
             # check if there are unopened feasible valves left
             unopened_feasible = False
             for valve in self.valves.values():
-                if valve.valve not in opened and self.is_feasible(valve):
+                if valve.valve not in journey.opened and self._is_feasible(valve):
                     unopened_feasible = True
                     break
             if not unopened_feasible:
                 # No more actions to take, return the current path
-                final_pressure = self.calculate_pressure(path, self.start_time)
-                path.append(("return", str(final_pressure)))
-                # print(f"No more actions to take from path {path}")
-                return path
+                journey.complete_journey(self.start_time)
+                return journey
             raise Exception(
-                "No more actions to take, but there are unopened feasible valves left"
+                "No more actions to take, but there are unopened feasible valves left. Logical error somewhere!"
             )
 
         # Find the best path of actions to take
-        best_pressure = 0
-        best_path = []
-        # print(f"Path: {path}, time left: {time_left}, next actions: {next_actions}")
-        for action, valve_b in next_actions:
-            new_path = list(path)
-            new_path.append((action, valve_b))
-            final_path = self.find_highest_pressure(new_path, time_left - 1)
-            final_pressure = int(final_path[-1][1])
-            if final_pressure > best_pressure:
-                best_pressure = final_pressure
-                best_path = final_path
-        return best_path if best_path else [("return", "0")]
+        best_journey = None
+        for new_journey in next_journeys:
+            complete_journey = self.find_highest_pressure(new_journey)
+            if not complete_journey.complete:
+                raise ValueError(f"Journey not complete, {complete_journey}")
+            if not best_journey:
+                best_journey = complete_journey
+                continue
+            if complete_journey > best_journey:
+                best_journey = complete_journey
+        # Intentionally returning incomplete journey if no best_journey
+        return best_journey if best_journey else journey
 
 
+@dataclass(frozen=True)
 class Valve:
-    def __init__(self, parse_string) -> None:
-        self.valve = ""
-        self.flow_rate = 0
-        self.tunnels: list[str] = []
-        self._parse(parse_string)
+    """One individual valve node"""
 
-    def _parse(self, parse_string) -> None:
+    valve: str
+    flow_rate: int
+    tunnels: tuple[str]
+
+    @classmethod
+    def parse(cls, parse_string: str) -> "Valve":
+        """Parse a string to create a Valve object
+
+        Args:
+            parse_string (str): String to parse in the format
+                "Valve AA has flow rate=1; tunnels lead to valves BB, CC"
+
+        Returns:
+            Valve: Valve object
+        """
         pattern = re.compile(
             r"Valve ([A-Z]{2}) has flow rate=(\d+); tunnels? leads? to valves? (.*)"
         )
         valve_details: tuple[str, str, str] = pattern.findall(parse_string)[0]
-        self.valve = valve_details[0]
-        self.flow_rate = int(valve_details[1])
-        self.tunnels = valve_details[2].split(", ")
+        valve = valve_details[0]
+        flow_rate = int(valve_details[1])
+        tunnels = tuple(valve_details[2].split(", "))
+        return cls(valve, flow_rate, tunnels)
 
     def __repr__(self):
         return f"Valve(valve={self.valve}, flow_rate={self.flow_rate}, tunnels={self.tunnels})"
@@ -204,16 +254,18 @@ def main_part1(
 
     valves: dict[str, Valve] = {}
     for line in lines:
-        valve = Valve(line)
+        valve = Valve.parse(line)
         valves[valve.valve] = valve
 
     time_left = 30
     # Start in valve AA
-    current_valve = valves["AA"]
-    spelunker = Spelunker(current_valve, valves, time_left)
-    path = spelunker.find_highest_pressure([("start", "AA")], time_left)
-    print(f"Path: {path}")
-    solution = path[-1][1]
+    spelunker = Spelunker(valves, time_left)
+    start_journey = Journey([("start", "AA")], time_left, valves)
+    best_journey = spelunker.find_highest_pressure(start_journey)
+    print(f"Path: {best_journey.path}")
+    if not best_journey.complete:
+        raise ValueError(f"Journey not complete, {best_journey}")
+    solution = best_journey.pressure
     return solution
 
 
